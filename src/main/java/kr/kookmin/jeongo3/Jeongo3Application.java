d7f6b465fabd07db2c80fcb9ffe4c1b47ab5892b
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2024-03-26 11:45:52 +0900

    spring initialize

---
+++ src/main/java/kr/kookmin/jeongo3/Jeongo3Application.java
@@ -0,0 +1,13 @@
+package kr.kookmin.jeongo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jeongo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jeongo3Application.class, args);
+	}
+
+}
